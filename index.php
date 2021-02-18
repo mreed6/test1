1217147cddf43a0e2b23dbb118182e34007a2d83
Reed <reed3@oakland.edu>
2021-02-18 16:38:56 -0500

    test 68

--- index.php
+++ index.php
@@ -31,7 +31,7 @@
     <section id="nav-bar">
 
         <nav class="navbar navbar-expand-lg navbar-light bg-primary">
-            <a>Back to Work Page</a>
+            <a><b>Back to Work Page</b></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
