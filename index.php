f1afed054a86efadf30c67559b4894407ca4d514
Reed <reed3@oakland.edu>
2021-02-18 14:43:53 -0500

    test 24

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+    <nav class="navbar navbar-expand-sm bg-primary navbar-dark fixed-top">
         <a class="navbar-brand" href="#">Logo</a>
         <ul class="navbar-nav">
             <li class="nav-item">
