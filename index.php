c12be3cacaae6963ce03bc2ae529161e028b96d5
Reed <reed3@oakland.edu>
2021-02-18 14:25:44 -0500

    test 14

--- index.php
+++ index.php
@@ -18,8 +18,8 @@
 </head>
 
 <body>
-    <div class="navbar" data-color="azure" data-background-color="blue">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light" data-color="azure" data-background-color="orange">
             <a class="navbar-brand" href="#">Navbar w/ text</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
