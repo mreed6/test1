32ad22824adfddc36e480ae5d28f0a15697fa78a
Reed <reed3@oakland.edu>
2021-02-18 14:58:19 -0500

    test 26

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
   <meta charset="utf-8" />
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
@@ -27,18 +28,24 @@
             </li>
         </ul>
     </nav>
-    <div class="container">
-        <h2>Width</h2>
-        <div class="w-auto p-3" style="background-color: #aaa;">Width : auto</div>
-        <br>
-        <div class="w-100 p-3" style="background-color: #aaa;">Width : 100%</div>
-        <br>
-        <div class="w-75 p-3" style="background-color: #aaa;">Width : 75%</div>
-        <br>
-        <div class="w-50 p-3" style="background-color: #aaa;">Width : 50%</div>
-        <br>
-        <div class="w-25 p-3" style="background-color: #aaa;">Width : 25%</div>
-        <br>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card ">
+                <div class="card-header ">
+                    <h5 class="card-title">Users Behavior</h5>
+                    <p class="card-category">24 Hours performance</p>
+                </div>
+                <div class="card-body ">
+                    <canvas id=chartHours width="400" height="100"></canvas>
+                </div>
+                <div class="card-footer ">
+                    <hr>
+                    <div class="stats">
+                        <i class="fa fa-history"></i> Updated 3 minutes ago
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
