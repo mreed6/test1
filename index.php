eefda233432a6a8250250ac072d36fc455897b03
mreed6 <43788492+mreed6@users.noreply.github.com>
2021-02-20 16:16:24 -0500

    test 92

--- index.php
+++ index.php
@@ -78,7 +78,7 @@
         $(window).on('click', function(event){
             // element over which click was made
             var clickOver = $(event.target)
-            if ($('.navbar .navbar-toggler').attr('aria-expanded') == 'true' && clickOver.closest('.navbar').length === 0) {
+            if ($('.navbar .navbar-toggler').attr('aria-expanded') === 'true' && clickOver.closest('.navbar').length === 0) {
                 // Click on navbar toggler button
                 $('button[aria-expanded="true"]').click();
             }
