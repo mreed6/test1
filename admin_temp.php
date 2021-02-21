8a9f5e551cb20350ea08da604fabb0a75753f006
Reed <reed3@oakland.edu>
2021-02-21 18:20:34 -0500

    test 110

--- admin_temp.php
+++ admin_temp.php
@@ -6,8 +6,8 @@
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
-    <link href="css/bootstrap.min.css" rel="stylesheet" />
-    <link href="css/paper-dashboard.css?v=2.0.1" rel="stylesheet" />
+    <link href="../RTWSafetySuite/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="../RTWSafetySuite/css/paper-dashboard.css?v=2.0.1" rel="stylesheet" />
     <link href="css/demo.css" rel="stylesheet" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
